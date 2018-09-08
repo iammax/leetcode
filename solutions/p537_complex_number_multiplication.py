1781357bad2e9a3a78e9a9ad84884fb3ab76bb54
Max Mathless <iammax64@gmail.com>
2018-09-08 11:14:19 -0400

    Impoved problem 537 drastically

--- solutions/p537_complex_number_multiplication.py
+++ solutions/p537_complex_number_multiplication.py
@@ -1,31 +1,8 @@
 class Solution(object):
     def complexNumberMultiply(self, a, b):
-        a1, b1 = parser(a)
-        a2, b2 = parser(b)
+        a1, b1 = map(int,a[:-1].split('+'))
+        a2, b2 = map(int,b[:-1].split('+'))
         outa = (a1*a2)-(b1*b2)
         outb = (a1*b2)+(b1*a2)
         out = str(outa)+'+' + str(outb) + 'i'
         return out
-        
-
-def parser(z):
-    a = ''
-    b = ''
-    dim = len(z)
-    counter = 0
-    mode = 1
-    while counter < dim:
-        here = z[counter]
-        if mode == 1:
-            if here != '+':
-                a += here
-            else:
-                mode = 2
-                if z[counter+1] == '-':
-                    b += '-'
-                    counter += 1
-        else:
-            if here != 'i':
-                b += here
-        counter += 1
-    return [int(a), int(b)]
