01d1991013c1ae02ceef493c10c038b23401738a
Max Mathless <iammax64@gmail.com>
2018-10-16 11:39:27 -0400

    Made problem 921 way faster and simpler

--- p921_minimum_add_parenthesis.py
+++ p921_minimum_add_parenthesis.py
@@ -1,34 +1,15 @@
 class Solution(object):
     def minAddToMakeValid(self, S):
-        if len(S) < 2: return len(S)
-        dim = len(S)
-        invalid = 0
-        counter = 0
-        dummy = False
-        finished = False
-        while dummy == False:
-            if len(S) == 1:
-                return invalid + 1
-            thisleft = S[counter]
-            if thisleft == ')':
-                invalid += 1
-                S = removeindex(S, counter)
+        invalids = 0
+        lefts = 0
+        rights = 0
+        for q in S:
+            if q == '(':
+                lefts += 1
             else:
-                subcounter = counter+1
-                subdummy = False
-                while subdummy == False:
-                    thisright = S[subcounter]
-                    if thisright == ')':
-                        S = removeindex(S, subcounter)
-                        S = removeindex(S, counter)
-                        subdummy = True
-                    subcounter += 1 
-                    if subcounter == len(S) and thisright == '(':
-                        subdummy = True
-                        dummy = True
-            counter += 0
-            if counter == len(S):
-                dummy = True
-        return len(S) - counter + invalid
-def removeindex(S, index):
-    return S[:index] + S[1+index:]
+                if lefts > 0:
+                    lefts -= 1
+                else:
+                    invalids += 1
+        print invalids, lefts, rights
+        return invalids + lefts + rights
