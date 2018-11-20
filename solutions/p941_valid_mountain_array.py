483c3c23d053374a619c49ff97b0f93f54c9f1b7
Max Mathless <iammax64@gmail.com>
2018-11-19 22:10:59 -0500

    Added alternate but equally fast version of p941

--- solutions/p941_valid_mountain_array.py
+++ solutions/p941_valid_mountain_array.py
@@ -17,3 +17,18 @@ class Solution(object):
                     return False
             prev = here
         return peaked
+
+#Alternate solution; same speed according to leetcode
+class Solution(object):
+    def validMountainArray(self, A):
+        if len(A) < 3:
+            return False
+        biggest = max(A)
+        if A.count(biggest) > 1:
+            return False
+        peak = A.index(biggest)
+        rise = A[:peak]
+        fall = A[peak+1:]
+        if len(rise) == 0 or len(fall) == 0 or len(rise) != len(set(rise)) or len(fall) != len(set(fall)):
+            return False
+        return rise == sorted(rise) and fall == sorted(fall)[::-1]
