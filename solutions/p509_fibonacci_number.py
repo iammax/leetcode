1e5c1a75529371239da974c35bc90b5364700152
Max Mathless <iammax64@gmail.com>
2019-01-25 16:57:08 -0500

    Updated p509 with math workaround

--- solutions/p509_fibonacci_number.py
+++ solutions/p509_fibonacci_number.py
@@ -6,4 +6,7 @@ class Solution(object):
             return 1
         else:
             return self.fib(N-1) + self.fib(N-2)
+#       If you want to cheat and use the formula which is way faster... use below
         
+#       phi = (1 + (5**.5))/2
+#       return int((phi**N - (1-phi)**N)/(5**.5))
