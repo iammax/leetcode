7f7acdadc397856bd8fc79accbfc97bd204bf51c
Max Mathless <iammax64@gmail.com>
2019-03-12 14:24:01 -0400

    Optimized #1005 so it's about 5% faster

--- solutions/p1005_maximize_after_negeations.py
+++ solutions/p1005_maximize_after_negeations.py
@@ -1,7 +1,18 @@
 class Solution(object):
     def largestSumAfterKNegations(self, A, K):
         while K > 0:
-            A = sorted(A)
-            A[0] = -A[0]
+            m = min(A)
+            index = A.index(m)
+            A[index] = -m
             K -= 1
         return sum(A)
+
+
+#slower version below
+#class Solution(object):
+#    def largestSumAfterKNegations(self, A, K):
+#        while K > 0:
+#            A = sorted(A)
+#            A[0] = -A[0]
+#            K -= 1
+#        return sum(A)
