c4ba3665324f2ec07dc7ed5a121c869fab49685a
Max Mathless <iammax64@gmail.com>
2019-05-20 11:38:25 -0400

    Optimization: Eliminated sorting the full list in each loop, increasing speed by 20%

--- solutions/p1046_last_stone_weight.py
+++ solutions/p1046_last_stone_weight.py
@@ -1,11 +1,25 @@
 class Solution(object):
     def lastStoneWeight(self, stones):
+        stones = sorted(stones)
         while len(stones) > 1:
-            stones = sorted(stones)
             biggest, second = stones[-1], stones[-2]
             if biggest > second:
-                print stones[:-2], [biggest-second]
-                stones = stones[:-2] + [biggest-second]
+                stones = stones[:-2]
+                newstone = biggest - second
+                dummy = False
+                counter = 0
+                while not dummy:
+                    try:
+                        here = stones[counter]
+                        if newstone <= here:
+                            stones = stones[:counter] + [newstone] + stones[counter:]
+                            dummy = True
+                        else:
+                            counter += 1
+                    
+                    except:
+                        dummy = True
+                        stones = stones + [newstone]
             else:
                 stones = stones[:-2]
         try:
