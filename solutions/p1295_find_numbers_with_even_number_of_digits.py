2f84410f03165d4f5653a32b34c94823378520ff
Max Mathless <iammax64@gmail.com>
2019-12-23 15:53:19 -0500

    Added an alternate method of solution for p1295

--- solutions/p1295_find_numbers_with_even_number_of_digits.py
+++ solutions/p1295_find_numbers_with_even_number_of_digits.py
@@ -1,11 +1,25 @@
-import math
+### Method 1: slightly better memory usage, slightly worse runtime, no imports
 
 class Solution(object):
     def findNumbers(self, nums):
         total = 0
         for num in nums:
-            digs = int(math.log10(num))
-            if digs%2 == 1:
+            st = str(num)
+            if len(st)%2 == 0:
                 total += 1
         return total
-            
+
+
+### Method 2: slightly better runtime, using an import, more purely mathematical
+
+#import math
+
+#class Solution(object):
+#    def findNumbers(self, nums):
+#        total = 0
+#        for num in nums:
+#            digs = int(math.log10(num))
+#            if digs%2 == 1:
+#                total += 1
+#        return total
+#            
