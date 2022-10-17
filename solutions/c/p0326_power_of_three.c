ccf381dae6d899f9b62f2659683410893b3ad9a6
Max <iammax64@gmail.com>
2022-10-17 12:40:41 -0400

    Added second method to p0326

--- solutions/c/p0326_power_of_three.c
+++ solutions/c/p0326_power_of_three.c
@@ -1,4 +1,8 @@
+
+
 bool isPowerOfThree(int n){
+    //method 1: climb up
+    /*
     long long unsigned x = 1;
     if(n<=0){return 0;}
     if (n<1){n = 1/n;}
@@ -6,5 +10,16 @@ bool isPowerOfThree(int n){
         if(n==x){return 1;}
         x*=3;
         if(x>n){return 0;}
+    }
+    */
+    
+    //method 2: climb down
+    if(n<=0){return 0;}
+    if(n==1){return 1;}
+    if (n<1){n = 1/n;}
+    while (1>0){
+        if (n%3!=0){return 0;}
+        n/=3;
+        if (n==1){return 1;}
     }
 }
